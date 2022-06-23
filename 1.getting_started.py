429d17c1ad5b44cdb8f4be4490591b48d16a2194
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-06-23 20:11:36 +0530

    Initial piece of code that allows us to get an idea

---
+++ 1.getting_started.py
@@ -0,0 +1,17 @@
+#Case -1-Usage of  first scipy tutorial
+
+from scipy import constants
+print(constans.liter)
+
+#Output : constants: SciPy offers a set of mathematical constants, one of them is liter which returns 1 liter as cubic meters.
+
+# You will learn more about constants in the next chapter.
+
+
+
+
+#Checking the scipy version
+
+from scipy import constants
+print(scipy.__version__)
+
