be907cebf5b664c9970a8ec833ede76f32840f4e
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-06-27 21:25:48 +0530

    info about root

--- 3.optimizers.py
+++ 3.optimizers.py
@@ -35,3 +35,27 @@ print(myroot.x)
 
 #Output : 
 # [-0.73908513]
+
+
+#Case-2- Root information
+
+from scipy.optimize import root
+from math import cos
+
+def eqn(x):
+  return x + cos(x)
+
+myroot = root(eqn, 0)
+
+print(myroot)
+
+#Output :
+#   fjac: array([[-1.]])
+#      fun: array([ 0.])
+#  message: 'The solution converged.'
+#     nfev: 9
+#      qtf: array([ -2.66786593e-13])
+#        r: array([-1.67361202])
+#   status: 1
+#  success: True
+#        x: array([-0.73908513])
