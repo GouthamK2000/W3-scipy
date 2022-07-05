751743803770a02998fe5ae45555da27c025499f
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-07-05 22:56:40 +0530

    Dijkstras algo

--- 5.Scipy_Graphs.py
+++ 5.Scipy_Graphs.py
@@ -44,3 +44,29 @@ arr = np.array([
 
 newarr=csr_matrix(arr)
 print(connected_components(newarr))
+
+# Dijkstra
+# Use the dijkstra method to find the shortest path in a graph from one element to another.
+
+# It takes following arguments:
+
+# return_predecessors: boolean (True to return whole path of traversal otherwise False).
+# indices: index of the element to return all paths from that element only.
+# limit: max weight of path.
+
+import numpy as np
+from scipy.sparse.csgraph import dijkstra
+from scipy.sparse import csr_matrix
+
+arr = np.array([
+  [0, 1, 2],
+  [1, 0, 0],
+  [2, 0, 0]
+])
+
+newarr = csr_matrix(arr)
+
+print(dijkstra(newarr, return_predecessors=True, indices=0))
+
+# Output:
+# (array([ 0.,  1.,  2.]), array([-9999,     0,     0], dtype=int32))
