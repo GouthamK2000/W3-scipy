379f5df70672e39b6b2bf5761888a9e9f0d5ec5c
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-07-06 21:53:42 +0530

    Graph data structure

--- 5.Scipy_Graphs.py
+++ 5.Scipy_Graphs.py
@@ -70,3 +70,27 @@ print(dijkstra(newarr, return_predecessors=True, indices=0))
 
 # Output:
 # (array([ 0.,  1.,  2.]), array([-9999,     0,     0], dtype=int32))
+
+#Floyd Warshall
+# Use the floyd_warshall() method to find shortest path between all pairs of elements.
+
+import numpy as np
+from scipy.sparse.csgraph import  floyd_warshall
+from scipy.sparse import csr_matrix
+
+arr = np.array([
+  [0, 1, 2],
+  [1, 0, 0],
+  [2, 0, 0]
+])
+
+newarr=csr_matrix(arr)
+print(floyd_warshall(newarr,return_predecessors=True))
+
+#Output:
+
+# (array([[ 0.,  1.,  2.],
+#        [ 1.,  0.,  3.],
+#        [ 2.,  3.,  0.]]), array([[-9999,     0,     0],
+#        [    1, -9999,     0],
+#        [    2,     0, -9999]], dtype=int32))
