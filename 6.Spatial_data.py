b47252defb8a1504ae2fd7dc4e30a37d51976853
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-07-10 23:21:53 +0530

    Convex hull

--- 6.Spatial_data.py
+++ 6.Spatial_data.py
@@ -30,3 +30,37 @@ plt.scatter(points[:, 0], points[:, 1], color='r')
 plt.show()
 
 #Outputs:  A polygon divided into triangles.
+
+
+# Convex Hull
+# A convex hull is the smallest polygon that covers all of the given points.
+
+# Use the ConvexHull() method to create a Convex Hull.
+
+import numpy as np
+from scipy.spatial import ConvexHull
+import matplotlib.pyplot as plt
+
+points = np.array([
+  [2, 4],
+  [3, 4],
+  [3, 0],
+  [2, 2],
+  [4, 1],
+  [1, 2],
+  [5, 0],
+  [3, 1],
+  [1, 2],
+  [0, 2]
+])
+
+hull = ConvexHull(points)
+hull_points = hull.simplices
+
+plt.scatter(points[:,0], points[:,1])
+for simplex in hull_points:
+  plt.plot(points[simplex,0], points[simplex,1], 'k-')
+
+plt.show()
+
+#Output : Draws a polygon, habving all other dots inside the polygon.It covers the outermost  4 points
