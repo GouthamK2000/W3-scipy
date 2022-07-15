d6bc4538234a4469717165aacb9a78a68b8251d0
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-07-15 21:30:12 +0530

    hamming distance

--- 6.Spatial_data.py
+++ 6.Spatial_data.py
@@ -144,3 +144,21 @@ print(res)
 
 #Output:   0.019419324309079777
 
+
+
+# Hamming Distance
+# Is the proportion of bits where two bits are difference.
+
+# It's a way to measure distance for binary sequences.
+
+
+from scipy.spatial.distance import hamming
+
+p1 = (True, False, True)
+p2 = (False, True, True)
+
+res = hamming(p1, p2)
+
+print(res)
+
+#Output : 0.666666666667
