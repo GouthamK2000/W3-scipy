eb155af2a2ffa18171aeda7e537ef0b826f9e4a2
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-07-17 23:24:52 +0530

    arrays1

--- 7.Scipy_Matlab_Arrays.py
+++ 7.Scipy_Matlab_Arrays.py
@@ -21,4 +21,37 @@ io.savemat('arr.mat', {"vec": arr})
 
 # Note: The example above saves a file name "arr.mat" on your computer.
 
-# To open the file, check out the "Import Data from Matlab Format" example below:
+
+
+# Import Data from Matlab Format
+# The loadmat() function allows us to import data from a Matlab file.
+
+# The function takes one required parameter:
+
+# filename - the file name of the saved data.
+
+# It will return a structured array whose keys are the variable names, and the corresponding values are the variable values.
+
+# Example
+
+from scipy import io
+import numpy as np
+
+arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9,])
+
+# Export:
+io.savemat('arr.mat', {"vec": arr})
+
+# Import:
+mydata = io.loadmat('arr.mat')
+
+print(mydata)
+
+#Output:
+
+#  {
+#    '__header__': b'MATLAB 5.0 MAT-file Platform: nt, Created on: Tue Sep 22 13:12:32 2020',
+#    '__version__': '1.0',
+#    '__globals__': [],
+#    'vec': array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]])
+#  }
