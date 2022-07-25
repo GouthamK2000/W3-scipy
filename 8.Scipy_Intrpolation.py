4a27963b692f223b56b10e8fc872fd4a90cbcdc0
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-07-25 22:19:33 +0530

    Spline interpolation

--- 8.Scipy_Intrpolation.py
+++ 8.Scipy_Intrpolation.py
@@ -35,3 +35,26 @@ newarr = interp_func(np.arange(2.1, 3, 0.1))
 print(newarr)
 
 #Output :  [5.2  5.4  5.6  5.8  6.   6.2  6.4  6.6  6.8]
+
+# Spline Interpolation
+# In 1D interpolation the points are fitted for a single curve whereas in Spline interpolation the points are fitted against a piecewise function defined with polynomials called splines.
+
+# The UnivariateSpline() function takes xs and ys and produce a callable funciton that can be called with new xs.
+
+# Piecewise function: A function that has different definition for different ranges.
+
+from scipy.interpolate import UnivariateSpline
+import numpy as np
+
+xs = np.arange(10)
+ys = xs**2 + np.sin(xs) + 1
+
+interp_func = UnivariateSpline(xs, ys)
+
+newarr = interp_func(np.arange(2.1, 3, 0.1))
+
+print(newarr)
+
+#Output : 
+#   [5.62826474 6.03987348 6.47131994 6.92265019 7.3939103  7.88514634
+#    8.39640439 8.92773053 9.47917082]
